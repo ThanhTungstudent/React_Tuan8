83317a6b56c8314bcb52cb51bfe45ed1aadf18c8
PNgocThach <thachpn03tcdb@gmail.com>
2025-04-10 16:40:15 +0700

    get api Overview

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React from "react";
 
-function Card() {
+import { CiShoppingCart } from "react-icons/ci";
+import { FaDollarSign } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
+
+function Card({ overview }) {
+  const iconMap = {
+    shopping_cart: <CiShoppingCart className="text-pink-600" size={24} />,
+    dollar: <FaDollarSign className="text-blue-600" size={24} />,
+    user: <FaRegUserCircle className="text-blue-600" size={24} />,
+  };
   return (
-    <div className='w-[350px] border h-[150px] p-5 rounded-[5px]'>
-        <div className='flex flex-row justify-between'>
-            <p>Titel</p>
+    <div
+      className={`w-[300px] border h-[180px] p-5 rounded-[5px] 
+    bg-${overview?.bgColor}`}
+    >
+      <div className="flex flex-row justify-between">
+        <p>{overview?.title}</p>
+        <button
+          className={`p-2 border rounded-md ${
+            overview.icon === "shopping_cart"
+              ? "border-pink-600"
+              : "border-blue-600"
+          }`}
+        >
+          {iconMap[overview?.icon]}
+        </button>
+      </div>
+      <div className="mt-5">
+        <p className="text-2xl text-black font-bold">{overview?.value}</p>
+        <div className="mt-5">
+          {overview?.change_type === "increase" ? (
             <div>
-                icon
+              <span className="text-green-600">▲ </span>
+              <span className="text-green-600">{overview?.change}</span>
+              <span className="text-gray-400"> {overview?.desciption}</span>
             </div>
+          ) : (
+            <div>
+              <span>▼ </span>
+              <span>{overview?.change}</span>
+              <span> {overview?.desciption}</span>
+            </div>
+          )}
         </div>
-        <div className='mt-5'>
-            <p className='text-2xl text-black font-bold'>price</p>
-            <p className='mt-5'>description</p>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Card/Card'
 
 function Overview() {
+  const [overviews, setOverviews] = useState()
+
+  useEffect(() => {
+    fetch("http://localhost:3000/overview")
+    .then((res) => res.json())
+    .then((data) => setOverviews(data))
+    .catch(((error) => console.error("Error fetching data:", error)
+
+    ))
+  }, [])
   return (
     <div className='w-full p-5 space-y-5' >
         <div className='flex flex-row gap-2'>
@@ -9,9 +19,11 @@ function Overview() {
         <h1 className='text-2xl font-bold text-black'>Overview</h1>
         </div>
         <div className='grid grid-cols-3 gap-5'>
-        <Card/>
-        <Card/>
-        <Card/>
+        {overviews && overviews.map((overview, index) => (
+          <div key={index} className='flex w-full items-center justify-center'>
+            <Card  overview={overview}/>
+          </div>
+        ))}
         </div>
         
     </div>
